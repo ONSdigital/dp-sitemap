5b9dff6c2fb8b7b779832287739487cdd60242f3
nimitsarup <nimitsarup@gmail.com>
2023-02-23 17:29:22 +0000

    more review comments

--- features/steps/steps.go
+++ features/steps/steps.go
@@ -6,13 +6,12 @@ import (
 	"os"
 	"strings"
 
-	"github.com/cucumber/godog"
-
 	assetmock "github.com/ONSdigital/dp-sitemap/assets/mock"
 	"github.com/ONSdigital/dp-sitemap/robotseo"
+	"github.com/cucumber/godog"
 )
 
-func (c *Component) RegisterSteps(ctx *godog.ScenarioContext) {	
+func (c *Component) RegisterSteps(ctx *godog.ScenarioContext) {
 	ctx.Step(`^i have the following robot\.json:$`, iHaveTheFollowingRobotjson)
 	ctx.Step(`^i invoke writejson with the sitemaps "([^"]*)"$`, c.iInvokeWritejsonWithTheSitemaps)
 	ctx.Step(`^the content of the resulting robots file must be$`, c.theContentOfTheResultingRobotsFileMustBe)
@@ -26,12 +25,12 @@ func iHaveTheFollowingRobotjson(arg1 *godog.DocString) error {
 	return nil
 }
 
-func (c *Component)iInvokeWritejsonWithTheSitemaps(arg1 string) error {
+func (c *Component) iInvokeWritejsonWithTheSitemaps(arg1 string) error {
 	fw := robotseo.RobotFileWriter{}
 	return fw.WriteRobotsFile(c.cfg, strings.Split(arg1, ","))
 }
 
-func (c *Component)theContentOfTheResultingRobotsFileMustBe(arg1 *godog.DocString) error {
+func (c *Component) theContentOfTheResultingRobotsFileMustBe(arg1 *godog.DocString) error {
 	b, err := os.ReadFile(c.cfg.RobotsFilePath)
 	if err != nil {
 		return err
