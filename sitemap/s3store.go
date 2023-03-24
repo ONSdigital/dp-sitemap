148435c97a28f270709b6157b0da4ce250eb3839
nimitsarup <nimitsarup@gmail.com>
2023-03-24 12:35:54 +0000

    review comment

--- sitemap/s3store.go
+++ sitemap/s3store.go
@@ -3,7 +3,6 @@ package sitemap
 import (
 	"fmt"
 	"io"
-	"path/filepath"
 	"strings"
 
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
@@ -28,12 +27,11 @@ func NewS3Store(client S3Client) *S3Store {
 }
 
 func (s *S3Store) SaveFile(name string, body io.Reader) error {
-	key := filepath.Base(name) // trim the path
 	bucket := s.client.BucketName()
 	_, err := s.client.Upload(&s3manager.UploadInput{
 		Body:   body,
 		Bucket: &bucket,
-		Key:    &key,
+		Key:    &name,
 	})
 	if err != nil {
 		return fmt.Errorf("failed to upload file to s3: %w", err)
