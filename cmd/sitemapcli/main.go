c65528618100bf2f405f9cb10ceaad23bfa8b3ae
Baratha Abeyrathne <barathaaberathne@gmail.com>
2023-04-21 14:55:40 +0100

    Code refactored

--- cmd/sitemapcli/main.go
+++ cmd/sitemapcli/main.go
@@ -4,130 +4,88 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"net/http"
 	"os"
 	"reflect"
+	"strings"
 
 	//"testing"
 
+	"github.com/ONSdigital/dp-api-clients-go/v2/zebedee"
+	"github.com/ONSdigital/dp-net/v2/awsauth"
+	dphttp "github.com/ONSdigital/dp-net/v2/http"
 	"github.com/ONSdigital/dp-sitemap/assets"
 	"github.com/ONSdigital/dp-sitemap/config"
 	"github.com/ONSdigital/dp-sitemap/robotseo"
 	"github.com/ONSdigital/dp-sitemap/sitemap"
-	"github.com/ONSdigital/dp-sitemap/sitemap/mock"
+	es710 "github.com/elastic/go-elasticsearch/v7"
 )
 
 // Config represents service configuration for dp-sitemap
 type FlagFields struct {
-	lang             string
 	robots_file_path string
 	api_url          string
 	sitemap_index    string
 	scroll_timeout   string
 	scroll_size      int
+	sitemap_path     string
+	zebedee_url      string
 }
 
 // test function for FlagFields
-// func test() bool {
-// 	fmt.Println("flagtest start..") //put log entry")
-
-// 	flagfields := FlagFields{
-// 		lang:             "lang",
-// 		robots_file_path: "/temp/robotfile.txt",
-// 	}
-// 	v := reflect.ValueOf(flagfields)
-// 	for i := 0; i < v.NumField(); i++ {
-// 		flagtest := v.Field(i).String()
-// 		fmt.Println("flagtest is " + flagtest) //put log entry
-// 		if flagtest == "" {
-// 			fmt.Println("flagtest is empty")
-// 			return false
-// 		} else if flagtest == "robots-file-path" {
-// 			fmt.Println("flagtest is robots-file-path")
-// 			return true
-// 		} else if flagtest == "lang" {
-// 			fmt.Println("flagtest is lang")
-// 			//check with language options
-// 			return true
-// 		} else {
-// 			fmt.Println("flagtest is lang")
-// 			return false
-// 		}
-
-// 	}
-// 	fmt.Println("flagtest end..") //put log entry")
-
-// 	return true
-
-// }
-
-// test function for FlagFields
 func validConfig(flagfields *FlagFields) bool {
-	fmt.Println("flagtest start..") //put log entry")
+	fmt.Println("flag validation started..") //put log entry
 
 	v := reflect.ValueOf(*flagfields)
-	//for i := 1; i < v.NumField(); i++ {
 	for i := 0; i < v.NumField(); i++ {
 		flagtest := v.Field(i).String()
-		fmt.Println("flagtest is " + flagtest) //put log entry
 		if flagtest == "" {
 			fmt.Println(v.Type().Field(i).Name + " is empty")
-			//logger
 			return false
 		} else {
 			fmt.Println(v.Type().Field(i).Name + " = " + v.Field(i).String())
 		}
 
 	}
-	fmt.Println("flagtest end..") //put log entry
+	fmt.Println("flagtest validation succesfull..") //put log entry
 
 	return true
 
 }
 
-// func (f *FlagFields) walkThru() error {
-
-// 	v := reflect.ValueOf(f).Elem()
-// 	fmt.Println(v)
-// 	for i := 0; i < v.NumField(); i++ {
-// 		found := false
-// 		fmt.Println(f.robots_file_path)
-// 		flag.Lookup(*&f.lang)
-// 		flag.Visit(func(f *flag.Flag) {
-// 			fmt.Println("heree")
-// 			if f.Value.String() == *lang {
-// 				return true
-// 			}
-// 		})
-// 		return found
-// 	}
-// 	fmt.Println("walkthru end")
-// 	return nil
-// }
-
-// func validateFlag(name string) bool {
-// 	found := false
-// 	flag.Visit(func(f *flag.Flag) {
-// 		if f.Value.String() == *lang {
-// 			found = true
-// 		}
-// 	})
-// 	return found
-// }
-
-// config struct
-// func validateFlagLooper(config string) {
-
-// }
-
-func main() {
+func validateCommandLines() bool {
 
 	commandline := FlagFields{}
-	flag.StringVar(&commandline.robots_file_path, "robots-file-path", "", "robotfile.txt")
-	flag.StringVar(&commandline.lang, "lang", "eng", "eng")
-	flag.StringVar(&commandline.api_url, "api-url", "", "OPENSEARCH_API_URL")
+	flag.StringVar(&commandline.robots_file_path, "robots-file-path", "robot_file.txt", "robotfile.txt")
+	flag.StringVar(&commandline.sitemap_path, "sitemap-file-path", "sitemap.xml", "sitemap.xml")
+	flag.StringVar(&commandline.api_url, "api-url", "", "")
+	flag.StringVar(&commandline.zebedee_url, "zebedee-url", "", "")
 	flag.StringVar(&commandline.sitemap_index, "sitemap-index", "", "OPENSEARCH_SITEMAP_INDEX")
 	flag.StringVar(&commandline.scroll_timeout, "scroll-timeout", "", "OPENSEARCH_SCROLL_TIMEOUT")
-	flag.IntVar(&commandline.scroll_size, "scroll-size", 5, "OPENSEARCH_SCROLL_TIMEOUT")
+	flag.IntVar(&commandline.scroll_size, "scroll-size", 10, "OPENSEARCH_SCROLL_SIZE")
+
+	flag.Parse()
+	if !validConfig(&commandline) {
+		flag.Usage = func() {
+			fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [options]\n", os.Args[0])
+			fmt.Fprintf(flag.CommandLine.Output(), "\nOptions:\n")
+			flag.PrintDefaults()
+		}
+		return false
+	} else {
+		return true
+	}
+}
+func main() {
+	//validate commandline - reduce this part as well
+	commandline := FlagFields{}
+	flag.StringVar(&commandline.robots_file_path, "robots-file-path", "robot_file.txt", "robotfile.txt")
+	flag.StringVar(&commandline.sitemap_path, "sitemap-file-path", "sitemap.xml", "sitemap.xml")
+	flag.StringVar(&commandline.api_url, "api-url", "", "")
+	flag.StringVar(&commandline.zebedee_url, "zebedee-url", "", "")
+	flag.StringVar(&commandline.sitemap_index, "sitemap-index", "", "OPENSEARCH_SITEMAP_INDEX")
+	flag.StringVar(&commandline.scroll_timeout, "scroll-timeout", "", "OPENSEARCH_SCROLL_TIMEOUT")
+	flag.IntVar(&commandline.scroll_size, "scroll-size", 10, "OPENSEARCH_SCROLL_SIZE")
 
 	flag.Parse()
 	if !validConfig(&commandline) {
@@ -138,56 +96,92 @@ func main() {
 		}
 		return
 	}
-
-	//Creating dp_robot_file.txt
-	robotseo.Init(assets.NewFromEmbeddedFilesystem())
-	robotFileWriter := robotseo.RobotFileWriter{}
-
 	cfg, err := config.Get()
+
 	if err != nil {
 		fmt.Println("Error retrieving config" + err.Error())
 		os.Exit(1)
+	} else {
+
+		GenerateSitemap(cfg, err, commandline)
+
+		//create robot.txt file
+		GenerateRobotFile(cfg, commandline)
 	}
-	cfg.RobotsFilePath = commandline.robots_file_path
-
-	if wErr := robotFileWriter.WriteRobotsFile(cfg, []string{}); wErr != nil {
-		fmt.Println("Error writing robot files", wErr.Error())
-		return
-	}
-
-	//Generate sitemap
-	//ctx, cancel := context.WithTimeout(context.Background(), cfg.SitemapGenerationTimeout)
-
-	saver := &mock.FileSaverMock{}
-	fetcher := &mock.FetcherMock{}
-	fetcher.HasWelshContentFunc = func(ctx context.Context, path string) bool { return false }
-
-	// fetcher.GetFullSitemapFunc = func(ctx context.Context) ([]string, error) {
-	// 	return []string{""}, errors.New("fetcher error")
-	// }
-
-	generator := sitemap.NewGenerator(fetcher, saver)
-
-	genErr := generator.MakeFullSitemap(context.Background())
-	if genErr != nil {
-		fmt.Println("Error writing robot files", genErr.Error())
-		return
-	}
-	fmt.Println("sitemap generation job complete")
-	//log.Info(ctx, "sitemap generation job complete", log.Data{"last_run": job.LastRun(), "next_run": job.NextRun(), "run_count": job.RunCount()})
 
 }
 
-// func TestFlagFields_walkThru(t *testing.T) {
-// 	f := FlagFields{value: ""}
-// 	err := f.walkThru()
-// 	if err == nil {
-// 		t.Error("Expected error but got nil")
-// 	}
+func GenerateSitemap(cfg *config.Config, err error, commandline FlagFields) {
+	//Create local file store
+	store := &sitemap.LocalStore{}
 
-// 	f2 := FlagFields{value: "some value"}
-// 	err = f2.walkThru()
-// 	if err != nil {
-// 		t.Errorf("Unexpected error: %v", err)
-// 	}
-// }
+	//Get ElasticSearch Clients
+	var transport http.RoundTripper = dphttp.DefaultTransport
+	if cfg.OpenSearchConfig.Signer {
+		//add SignerRegion,SignerService
+		transport, err = awsauth.NewAWSSignerRoundTripper(cfg.OpenSearchConfig.APIURL, cfg.OpenSearchConfig.SignerFilename, cfg.OpenSearchConfig.SignerRegion, cfg.OpenSearchConfig.SignerService, awsauth.Options{TlsInsecureSkipVerify: cfg.OpenSearchConfig.TLSInsecureSkipVerify})
+		if err != nil {
+			fmt.Printf("failed to save file")
+			return
+		}
+	}
+
+	//Get rawClient using arg -api-url
+	rawClient, err := es710.NewClient(es710.Config{
+		Addresses: []string{commandline.api_url},
+		Transport: transport,
+	})
+	if err != nil {
+		return
+	}
+
+	//Get zebedeeClient using arg -zebedee-url
+	zebedeeClient := zebedee.New(commandline.zebedee_url)
+
+	generator := sitemap.NewGenerator(
+		sitemap.WithFetcher(sitemap.NewElasticFetcher(
+			rawClient,
+			cfg,
+			zebedeeClient,
+		)),
+		sitemap.WithAdder(&sitemap.DefaultAdder{}),
+		sitemap.WithFileStore(store),
+		sitemap.WithFullSitemapFiles(map[config.Language]string{
+			config.English: commandline.sitemap_path + "_eng",
+			config.Welsh:   commandline.sitemap_path + "_welsh",
+		}),
+	)
+
+	//Generating sitemap
+	genErr := generator.MakeFullSitemap(context.Background())
+	if genErr != nil {
+		fmt.Println("Error writing sitemappp file", genErr.Error())
+		return
+	}
+	fmt.Println("sitemap generation job complete")
+}
+
+func GenerateRobotFile(cfg *config.Config, commandline FlagFields) {
+
+	robotseo.Init(assets.NewFromEmbeddedFilesystem())
+	robotFileWriter := robotseo.RobotFileWriter{}
+	cfg.RobotsFilePath = map[config.Language]string{
+		config.English: commandline.robots_file_path,
+	}
+
+	store := &sitemap.LocalStore{}
+
+	cfg.OpenSearchConfig.APIURL = commandline.api_url
+	cfg.OpenSearchConfig.ScrollSize = commandline.scroll_size
+	cfg.OpenSearchConfig.Signer = true
+
+	body := robotFileWriter.GetRobotsFileBody(config.Language(config.English), cfg.SitemapLocalFile)
+	fmt.Printf("robot file path is: %v", commandline.robots_file_path)
+
+	saveErr := store.SaveFile(commandline.robots_file_path, strings.NewReader(body))
+
+	if saveErr != nil {
+		fmt.Println("failed to save file")
+		return
+	}
+}
--- cmd/sitemapcli/main_test.go
+++ cmd/sitemapcli/main_test.go
@@ -2,48 +2,42 @@ package main
 
 import (
 	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestValidConfig(t *testing.T) {
 	// Define test cases
-	tests := []struct {
-		name       string
-		flagfields FlagFields
-		expected   bool
-	}{
-		{
-			name: "All fields filled",
-			flagfields: FlagFields{
-				robots_file_path: "value1",
-				lang:             "value2",
-				api_url:          "value3",
-				sitemap_index:    "abxx",
-				scroll_timeout:   "1000",
-				scroll_size:      2,
-			},
-			expected: true,
-		},
-		{
-			name: "Empty field",
-			flagfields: FlagFields{
-				robots_file_path: "value1",
-				lang:             "value2",
-				api_url:          "value3",
-				sitemap_index:    "",
-				scroll_timeout:   "",
-				scroll_size:      2,
-			},
-			expected: false,
-		},
-	}
-
-	// Run test cases
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			result := validConfig(&tc.flagfields)
-			if result != tc.expected {
-				t.Errorf("Expected %v, but got %v", tc.expected, result)
-			}
+	Convey("when all the args filled", t, func() {
+		testdata := FlagFields{
+			robots_file_path: "robot_file.txt",
+			api_url:          "test.api.url",
+			sitemap_index:    "2",
+			scroll_timeout:   "1000",
+			scroll_size:      2,
+			zebedee_url:      "test.zeebedee.url",
+			sitemap_path:     "/path",
+		}
+		Convey("Then the args are valid", func() {
+			result := validConfig(&testdata)
+			So(result, ShouldBeTrue)
 		})
-	}
+	})
+	//sitemap_index is missing
+	Convey("when some args are missing", t, func() {
+		tetestdata := FlagFields{
+			robots_file_path: "robot_file.txt",
+			api_url:          "test.api.url",
+			sitemap_index:    "",
+			scroll_timeout:   "200",
+			scroll_size:      2,
+			zebedee_url:      "test.zeebedee.url",
+			sitemap_path:     "/path",
+		}
+		Convey("Then the args are invalid", func() {
+
+			result := validConfig(&tetestdata)
+			So(result, ShouldBeFalse)
+		})
+	})
 }
