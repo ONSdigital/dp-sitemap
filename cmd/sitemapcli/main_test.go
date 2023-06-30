e8e0ef5e95d3abdb182c5bace53fe2ab5a12ad32
Dionisi Lola <dionisislolas@gmail.com>
2023-06-30 15:04:10 +0100

    refactore tests

--- cmd/sitemapcli/main_test.go
+++ cmd/sitemapcli/main_test.go
@@ -50,6 +50,7 @@ func TestValidConfig(t *testing.T) {
 }
 
 func TestLoadStaticSitemap(t *testing.T) {
+	expectedUrlset := expectedUrlSetEnglish()
 	Convey("when loading english static sitemap", t, func() {
 		store := mock.FileStoreMock{}
 		buf := new(bytes.Buffer)
@@ -61,45 +62,7 @@ func TestLoadStaticSitemap(t *testing.T) {
 		err := loadStaticSitemap(context.Background(), "test_sitemap_en", "sitemap_en.json", cfg.DpOnsURLHostNameEn, cfg.DpOnsURLHostNameCy, "cy", &store)
 		urlSet := &sitemap.UrlsetReader{}
 		xml.Unmarshal(buf.Bytes(), urlSet)
-		expectedUrlset := &sitemap.UrlsetReader{
-			XMLName: xml.Name{Space: "http://www.sitemaps.org/schemas/sitemap/0.9", Local: "urlset"},
-			Xmlns:   "http://www.sitemaps.org/schemas/sitemap/0.9",
-			URL: []sitemap.URLReader{
-				{
-					XMLName: xml.Name{Space: "http://www.sitemaps.org/schemas/sitemap/0.9", Local: "url"},
-					Loc:     "https://dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle1",
-					Lastmod: "01-01-2023",
-					Alternate: &sitemap.AlternateURLReader{
-						XMLName: xml.Name{Space: "http://www.w3.org/1999/xhtml", Local: "link"},
-						Rel:     "alternate",
-						Lang:    "cy",
-						Link:    "https://cy.dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle1",
-					},
-				},
-				{
-					XMLName: xml.Name{Space: "http://www.sitemaps.org/schemas/sitemap/0.9", Local: "url"},
-					Loc:     "https://dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle2",
-					Lastmod: "01-01-2023",
-					Alternate: &sitemap.AlternateURLReader{
-						XMLName: xml.Name{Space: "http://www.w3.org/1999/xhtml", Local: "link"},
-						Rel:     "alternate",
-						Lang:    "cy",
-						Link:    "https://cy.dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle2",
-					},
-				},
-				{
-					XMLName: xml.Name{Space: "http://www.sitemaps.org/schemas/sitemap/0.9", Local: "url"},
-					Loc:     "https://dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle3",
-					Lastmod: "01-01-2023",
-					Alternate: &sitemap.AlternateURLReader{
-						XMLName: xml.Name{Space: "http://www.w3.org/1999/xhtml", Local: "link"},
-						Rel:     "alternate",
-						Lang:    "cy",
-						Link:    "https://cy.dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle3",
-					},
-				},
-			},
-		}
+
 		Convey("There should be no error", func() {
 			So(err, ShouldBeNil)
 		})
@@ -119,45 +82,7 @@ func TestLoadStaticSitemap(t *testing.T) {
 		err := loadStaticSitemap(context.Background(), "test_sitemap_cy", "sitemap_cy.json", cfg.DpOnsURLHostNameCy, cfg.DpOnsURLHostNameEn, "en", &store)
 		urlSet := &sitemap.UrlsetReader{}
 		xml.Unmarshal(buf.Bytes(), urlSet)
-		expectedUrlset := &sitemap.UrlsetReader{
-			XMLName: xml.Name{Space: "http://www.sitemaps.org/schemas/sitemap/0.9", Local: "urlset"},
-			Xmlns:   "http://www.sitemaps.org/schemas/sitemap/0.9",
-			URL: []sitemap.URLReader{
-				{
-					XMLName: xml.Name{Space: "http://www.sitemaps.org/schemas/sitemap/0.9", Local: "url"},
-					Loc:     "https://cy.dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle1",
-					Lastmod: "01-01-2023",
-					Alternate: &sitemap.AlternateURLReader{
-						XMLName: xml.Name{Space: "http://www.w3.org/1999/xhtml", Local: "link"},
-						Rel:     "alternate",
-						Lang:    "en",
-						Link:    "https://dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle1",
-					},
-				},
-				{
-					XMLName: xml.Name{Space: "http://www.sitemaps.org/schemas/sitemap/0.9", Local: "url"},
-					Loc:     "https://cy.dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle2",
-					Lastmod: "01-01-2023",
-					Alternate: &sitemap.AlternateURLReader{
-						XMLName: xml.Name{Space: "http://www.w3.org/1999/xhtml", Local: "link"},
-						Rel:     "alternate",
-						Lang:    "en",
-						Link:    "https://dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle2",
-					},
-				},
-				{
-					XMLName: xml.Name{Space: "http://www.sitemaps.org/schemas/sitemap/0.9", Local: "url"},
-					Loc:     "https://cy.dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle3",
-					Lastmod: "01-01-2023",
-					Alternate: &sitemap.AlternateURLReader{
-						XMLName: xml.Name{Space: "http://www.w3.org/1999/xhtml", Local: "link"},
-						Rel:     "alternate",
-						Lang:    "en",
-						Link:    "https://dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle3",
-					},
-				},
-			},
-		}
+		expectedUrlset = expectedUrlSetWelsh()
 		Convey("There should be no error", func() {
 			So(err, ShouldBeNil)
 		})
@@ -165,4 +90,138 @@ func TestLoadStaticSitemap(t *testing.T) {
 			So(urlSet, ShouldResemble, expectedUrlset)
 		})
 	})
+}
+
+func TestLoadStaticSitemapV2(t *testing.T) {
+	Convey("given we have static sitemap file sitemap_en.json", t, func() {
+
+		Convey("when loading english static sitemap", func() {
+			store := mock.FileStoreMock{}
+			buf := new(bytes.Buffer)
+			store.SaveFileFunc = func(name string, body io.Reader) error {
+				io.Copy(buf, body)
+				return nil
+			}
+			cfg, _ := config.Get()
+			err := loadStaticSitemap(context.Background(), "test_sitemap_en", "sitemap_en.json", cfg.DpOnsURLHostNameEn, cfg.DpOnsURLHostNameCy, "cy", &store)
+			urlSet := &sitemap.UrlsetReader{}
+			xml.Unmarshal(buf.Bytes(), urlSet)
+
+			Convey("There should be no error", func() {
+				So(err, ShouldBeNil)
+			})
+
+			Convey("We should have the correct content loaded", func() {
+				So(urlSet, ShouldResemble, expectedUrlSetEnglish())
+			})
+		})
+	})
+
+	Convey("given we have static sitemap file sitemap_cy.json", t, func() {
+
+		Convey("when loading welsh static sitemap", func() {
+			store := mock.FileStoreMock{}
+			buf := new(bytes.Buffer)
+			store.SaveFileFunc = func(name string, body io.Reader) error {
+				io.Copy(buf, body)
+				return nil
+			}
+			cfg, _ := config.Get()
+			err := loadStaticSitemap(context.Background(), "test_sitemap_cy", "sitemap_cy.json", cfg.DpOnsURLHostNameCy, cfg.DpOnsURLHostNameEn, "en", &store)
+			urlSet := &sitemap.UrlsetReader{}
+			xml.Unmarshal(buf.Bytes(), urlSet)
+
+			Convey("There should be no error", func() {
+				So(err, ShouldBeNil)
+			})
+
+			Convey("We should have the correct content loaded", func() {
+				So(urlSet, ShouldResemble, expectedUrlSetWelsh())
+			})
+		})
+	})
+}
+
+func expectedUrlSetEnglish() *sitemap.UrlsetReader {
+	return &sitemap.UrlsetReader{
+		XMLName: xml.Name{Space: "http://www.sitemaps.org/schemas/sitemap/0.9", Local: "urlset"},
+		Xmlns:   "http://www.sitemaps.org/schemas/sitemap/0.9",
+		URL: []sitemap.URLReader{
+			{
+				XMLName: xml.Name{Space: "http://www.sitemaps.org/schemas/sitemap/0.9", Local: "url"},
+				Loc:     "https://dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle1",
+				Lastmod: "01-01-2023",
+				Alternate: &sitemap.AlternateURLReader{
+					XMLName: xml.Name{Space: "http://www.w3.org/1999/xhtml", Local: "link"},
+					Rel:     "alternate",
+					Lang:    "cy",
+					Link:    "https://cy.dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle1",
+				},
+			},
+			{
+				XMLName: xml.Name{Space: "http://www.sitemaps.org/schemas/sitemap/0.9", Local: "url"},
+				Loc:     "https://dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle2",
+				Lastmod: "01-01-2023",
+				Alternate: &sitemap.AlternateURLReader{
+					XMLName: xml.Name{Space: "http://www.w3.org/1999/xhtml", Local: "link"},
+					Rel:     "alternate",
+					Lang:    "cy",
+					Link:    "https://cy.dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle2",
+				},
+			},
+			{
+				XMLName: xml.Name{Space: "http://www.sitemaps.org/schemas/sitemap/0.9", Local: "url"},
+				Loc:     "https://dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle3",
+				Lastmod: "01-01-2023",
+				Alternate: &sitemap.AlternateURLReader{
+					XMLName: xml.Name{Space: "http://www.w3.org/1999/xhtml", Local: "link"},
+					Rel:     "alternate",
+					Lang:    "cy",
+					Link:    "https://cy.dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle3",
+				},
+			},
+		},
+	}
+}
+
+func expectedUrlSetWelsh() *sitemap.UrlsetReader {
+	return &sitemap.UrlsetReader{
+		XMLName: xml.Name{Space: "http://www.sitemaps.org/schemas/sitemap/0.9", Local: "urlset"},
+		Xmlns:   "http://www.sitemaps.org/schemas/sitemap/0.9",
+		URL: []sitemap.URLReader{
+			{
+				XMLName: xml.Name{Space: "http://www.sitemaps.org/schemas/sitemap/0.9", Local: "url"},
+				Loc:     "https://cy.dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle1",
+				Lastmod: "01-01-2023",
+				Alternate: &sitemap.AlternateURLReader{
+					XMLName: xml.Name{Space: "http://www.w3.org/1999/xhtml", Local: "link"},
+					Rel:     "alternate",
+					Lang:    "en",
+					Link:    "https://dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle1",
+				},
+			},
+			{
+				XMLName: xml.Name{Space: "http://www.sitemaps.org/schemas/sitemap/0.9", Local: "url"},
+				Loc:     "https://cy.dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle2",
+				Lastmod: "01-01-2023",
+				Alternate: &sitemap.AlternateURLReader{
+					XMLName: xml.Name{Space: "http://www.w3.org/1999/xhtml", Local: "link"},
+					Rel:     "alternate",
+					Lang:    "en",
+					Link:    "https://dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle2",
+				},
+			},
+			{
+				XMLName: xml.Name{Space: "http://www.sitemaps.org/schemas/sitemap/0.9", Local: "url"},
+				Loc:     "https://cy.dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle3",
+				Lastmod: "01-01-2023",
+				Alternate: &sitemap.AlternateURLReader{
+					XMLName: xml.Name{Space: "http://www.w3.org/1999/xhtml", Local: "link"},
+					Rel:     "alternate",
+					Lang:    "en",
+					Link:    "https://dp.aws.onsdigital.uk/economy/environmentalaccounts/articles/testarticle3",
+				},
+			},
+		},
+	}
 }
